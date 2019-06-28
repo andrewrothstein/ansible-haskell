41ea9919fd7e6a961715f07d99e96093f009bca9
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-06-27 22:48:10 -0500

    up to v2.1.1

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-VER='1.9.3'
+VER='2.1.1'
 DIR=~/Downloads
 MIRROR=https://github.com/commercialhaskell/stack/releases/download/v${VER}
 
@@ -8,17 +8,15 @@ dl()
     OS=$1
     PLATFORM=$2
     URL=$MIRROR/stack-${VER}-${OS}-${PLATFORM}.tar.gz.sha256
-    printf "# $URL\n"
-    printf "$OS-$PLATFORM: sha256:"
-    curl -SsL $URL | awk '{print $1}'
+    printf "    # %s\n" $URL
+    printf "    %s-%s: sha256:%s\n" $OS $PLATFORM `curl -SsL $URL | awk '{print $1}'`
 }
 
-
+printf "  '%s':\n" $VER
 dl freebsd x86_64
 dl linux aarch64
 dl linux arm
 dl linux i386
-dl linux x86_64-gmp4
 dl linux x86_64-static
 dl linux x86_64
 dl windows i386
