e6e8c71f7d05b97d051094b5443f82d0bee67991
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-09-22 19:55:40 -0500

    up to v2.1.3. upgrade dep vers. delint.

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env sh
-VER='2.1.1'
+VER='2.1.3'
 DIR=~/Downloads
 MIRROR=https://github.com/commercialhaskell/stack/releases/download/v${VER}
 
 dl()
 {
-    OS=$1
-    PLATFORM=$2
-    URL=$MIRROR/stack-${VER}-${OS}-${PLATFORM}.tar.gz.sha256
-    printf "    # %s\n" $URL
-    printf "    %s-%s: sha256:%s\n" $OS $PLATFORM `curl -SsL $URL | awk '{print $1}'`
+    local os=$1
+    local arch=$2
+    local platform="${os}-${arch}"
+    local url=$MIRROR/stack-${VER}-${platform}.tar.gz.sha256
+    printf "    # %s\n" $url
+    printf "    %s: sha256:%s\n" $platform $(curl -SsL $url | awk '{print $1}')
 }
 
 printf "  '%s':\n" $VER
